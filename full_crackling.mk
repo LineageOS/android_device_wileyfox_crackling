c1dd1a790e93dcf7ead3164f5bc4cd160c159d1e
Ricardo Cerqueira <ricardo@cyngn.com>
2015-05-15 16:24:51 -0700

    all the bits

--- full_crackling.mk
+++ full_crackling.mk
@@ -15,6 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from crackling device
