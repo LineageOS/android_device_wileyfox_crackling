f944bb7bcfe364d590897d4a240121d0add31ddc
Ethan Chen <intervigil@gmail.com>
2015-06-24 12:59:37 -0700

    crackling: Remove duplicate kernel flags
    * Already set in msm8916-common
    
    Change-Id: Ic53041c59f3edaf379ca109e86349665ff3f4997

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,10 +20,6 @@ DEVICE_PATH := device/wileyfox/crackling
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Kernel
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_CONFIG := cyanogenmod_crackling-64_defconfig
