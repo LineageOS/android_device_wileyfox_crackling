2bb438f94a7fc0a8751c751830fb2674c4e56913
Ethan Chen <intervigil@gmail.com>
2015-07-14 15:42:41 -0700

    crackling: Move USB composition script to common
    Change-Id: Ic294e3e012189839bfcd1f6d520329fbbf715d78

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,11 +9,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
