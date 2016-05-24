7c90575a61a42db62dec517e9407136a07af1875
Ethan Chen <intervigil@gmail.com>
2016-05-24 12:06:27 -0700

    crackling: Shift to new device fragment model
    * Use make fragments for various subsystems
    * Move SSR makefile to msm8916-common
    
    Change-Id: I2a002f24c308481664b516a761b7978292c3efca

---
+++ product/keylayouts.mk
@@ -0,0 +1,4 @@
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
