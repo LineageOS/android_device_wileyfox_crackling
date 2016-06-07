0188e93c09ef3add4042e33857c20427610493cb
Ethan Chen <intervigil@gmail.com>
2016-06-07 10:04:15 -0700

    crackling: Enable CNE, DPM, QC location
    Change-Id: Iae6a2bd5b78367779afa2fb900968e77cf7efefd

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -12,3 +12,8 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=1 \
+    ro.gps.agps_provider=1
--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -12,7 +12,5 @@ ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
-    $(LOCAL_PATH)/configs/data/NsrmConfiguration.xml:system/etc/dpm/nsrm/NsrmConfiguration.xml \
-    $(LOCAL_PATH)/configs/data/dpm.conf:system/etc/dpm/dpm.conf
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
 endif
