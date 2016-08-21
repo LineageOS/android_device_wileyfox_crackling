51a7f5726b953235ae31c5d701795f21a3116e40
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-21 18:37:28 +0300

    crackling: Correct indentation in extract-files.sh
    * Let's be clean
    
    Change-Id: I7ff4c8c637a31c17b1207ebfa6e0c2195b383069

--- extract-files.sh
+++ extract-files.sh
@@ -34,19 +34,19 @@ fi
 . "$HELPER"
 
 if [ $# -eq 0 ]; then
-  SRC=adb
+    SRC=adb
 else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
+    if [ $# -eq 1 ]; then
+        SRC=$1
+    else
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
+    fi
 fi
 
 # Initialize the helper
