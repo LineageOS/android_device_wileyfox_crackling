eaf0ee75fd88b3ccdaff6b182d7e75371d7126e6
luk1337 <luk1337@cyanogenmod.org>
2016-11-21 09:16:47 +0000

    crackling: sensors: bst: Do not include buildversion.mk
    * We don't need to regenerate version folder every build.
    
    Change-Id: I1d073a57b8a4673083251a3a69cb7f7ab3c81e3b

--- sensors/bst/hal/Android.mk
+++ sensors/bst/hal/Android.mk
@@ -60,7 +60,6 @@ LOCAL_PRELINK_MODULE := false
 include $(LOCAL_PATH)/../tools/l8150_config.mk
 
 include $(LOCAL_PATH)/../tools/options.mk
-include $(LOCAL_PATH)/../tools/buildversion.mk
 
 ifeq (true, $(bmp_support))
 LOCAL_SRC_FILES += BstSensorPressure.cpp
