d46669a8f6aca0a2b175b708118504d6ac1d96cc
Scott Warner <Tortel1210@gmail.com>
2017-09-13 19:25:15 -0400

    crackling: Switch to permissive
    Change-Id: Ia4c979c92f6c2c0f6ad384922b6f8d43a2d1b100

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,3 +5,4 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_CONFIG := lineageos_crackling_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
