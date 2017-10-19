50e89e56bdb297d14857c407a7b80e04a2238ae1
Arvin Quilao <arquilao@gmail.com>
2017-10-19 07:46:32 +0000

    crackling: Fix TARGET_RECOVERY_DEVICE_DIRS path
    Change-Id: Ic83be7c6dadac7ac738a9c6141cd9ecfc1eb5d2e

--- board/twrp.mk
+++ board/twrp.mk
@@ -1,5 +1,5 @@
 ifeq ($(WITH_TWRP),true)
-TARGET_RECOVERY_DEVICE_DIRS += device/yu/lettuce/twrp
+TARGET_RECOVERY_DEVICE_DIRS += device/wileyfox/crackling/twrp
 TW_INCLUDE_CRYPTO := true
 TW_THEME := portrait_hdpi
 endif
