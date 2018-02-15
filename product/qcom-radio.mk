928a0d4b1fffc7c7e3e826b8a47c541e2d99eaad
jrior001 <jriordan001@gmail.com>
2018-02-14 21:46:02 -0500

    crackling: move configs to vendor
    Change-Id: I017af4a93f218af464890558e86d40a3d8c76f73

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -9,6 +9,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RIL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/vendor/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/vendor/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/vendor/etc/data/dsi_config.xml
