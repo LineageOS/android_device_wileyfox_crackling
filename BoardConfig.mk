d62497ce0cd1771cedf1581a176da2b3584bcb23
Michael Bestas <mkbestas@lineageos.org>
2018-03-22 19:25:57 +0200

    crackling: Remove dexpreopt configuration
    * It's set globally now
    
    Change-Id: Ie61179cd898778190bcbec15779dbdca4070bfba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,16 +31,6 @@ BOARD_CAMERA_SENSORS := s5k5e2_olq5f19 s5k3m2_olqba20
 TARGET_USE_VENDOR_CAMERA_EXT := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
-# Dexopt
-ifeq ($(HOST_OS),linux)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
-        ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
-            WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-        endif
-    endif
-endif
-
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33553920
