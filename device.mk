5e801bee0732ebbf943a5ca22e9ee63e295d0dfa
Michael Bestas <mkbestas@lineageos.org>
2018-06-14 17:16:07 +0300

    crackling: Move keylayouts to vendor
    Change-Id: Id95f014cad3200b23b48861a72dd71e5ed2ec35a

--- device.mk
+++ device.mk
@@ -78,8 +78,8 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/vendor/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/vendor/usr/keylayout/qpnp_pon.kl
 
 # Lights
 PRODUCT_PACKAGES += \
