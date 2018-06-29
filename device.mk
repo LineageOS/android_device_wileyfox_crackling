a8312643808e4006e2bf39bb389298b91eae407e
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    crackling: Remove camera.device@1.0-impl from PRODUCT_PACKAGES
    * camera.device@1.0-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3

--- device.mk
+++ device.mk
@@ -49,8 +49,7 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    android.hardware.camera.provider@2.4-impl
 
 # Display
 PRODUCT_PACKAGES += \
