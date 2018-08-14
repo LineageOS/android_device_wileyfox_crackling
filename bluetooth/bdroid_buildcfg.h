d456d5d01fa2526216bc86f87a70bc61fe218900
Arne Coucheron <arco68@gmail.com>
2018-08-14 13:30:35 +0200

    crackling: Disable BLE_VND_INCLUDED for now
    If enabled, crashes the stack when bluetooth is turned on.
    
    Change-Id: I478970f153f8d8d6af44de6e0b21e68b4cb33a37

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,6 @@
 
 #define BTM_DEF_LOCAL_NAME        "Wileyfox Swift"
 
-#define BLE_VND_INCLUDED          TRUE
 #define BLUETOOTH_QTI_SW          TRUE
 #define BT_CLEAN_TURN_ON_DISABLED TRUE
 #define MAX_ACL_CONNECTIONS       7
