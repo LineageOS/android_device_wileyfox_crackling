ef8dcbe720385645eda0f3d1bc75346864cafd72
Dev4Perf <34247752+dev4perf@users.noreply.github.com>
2018-09-04 10:10:55 +0300

    enable adb on boot

---
+++ default-prop.mk
@@ -0,0 +1,15 @@
+# USB ID
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.usb.vid=2717 \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=ff60 \
+    ro.usb.id.mtp_adb=ff68 \
+    ro.usb.id.ptp=ff10 \
+    ro.usb.id.ptp_adb=ff18 \
+    ro.usb.id.ums=ff20 \
+    ro.usb.id.ums_adb=ff28
+
+# Debug
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=0 
