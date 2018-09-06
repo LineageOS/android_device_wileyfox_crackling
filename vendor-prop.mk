e928b00c42509005d9a9e4df7281a29e26af8fb8
Dev4Perf <34247752+dev4perf@users.noreply.github.com>
2018-09-06 17:54:30 +0300

    crackling: Set properties to make acdbloader loads acdb files from vendor

--- vendor-prop.mk
+++ vendor-prop.mk
@@ -1,5 +1,12 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+persist.audio.calfile0=/vendor/etc/acdbdata/QRD/QRD_Bluetooth_cal.acdb \
+persist.audio.calfile1=/vendor/etc/acdbdata/QRD/QRD_General_cal.acdb \
+persist.audio.calfile2=/vendor/etc/acdbdata/QRD/QRD_Global_cal.acdb \
+persist.audio.calfile3=/vendor/etc/acdbdata/QRD/QRD_Handset_cal.acdb \
+persist.audio.calfile4=/vendor/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
+persist.audio.calfile5=/vendor/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
+persist.audio.calfile6=/vendor/etc/acdbdata/QRD/QRD_Speaker_cal.acdb \
 ro.vendor.audio.sdk.fluencetype=fluence \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
