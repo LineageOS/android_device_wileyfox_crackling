32decb04f2cbd84d7935639fda1e65f55cee4568
Michael Bestas <mkbestas@lineageos.org>
2019-01-23 02:16:15 +0200

    crackling: Remove leftover wifi config symlink
    Change-Id: I2c35955a57e181117d72d70925368a03ac4a9e93

--- Android.mk
+++ Android.mk
@@ -15,16 +15,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), crackling)
-
+ifeq ($(TARGET_DEVICE),crackling)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-
-# Create a link for the WCNSS config file, which ends up as a writable
-# version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
-
 endif
