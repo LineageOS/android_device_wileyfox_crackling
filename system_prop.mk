1c399438adff87d31aee3af87ff55eb1e4df6172
Nolen Johnson <johnsonnolen@gmail.com>
2020-03-31 18:08:55 +0200

    crackling: system-properties: Migrate from system.prop to system_prop.mk
    * The build system auto-including system.prop will be deprecated in the near future, so migrate to setting all system propeties with a makefile.
    
    Change-Id: Ia640d4a8adf2eaa68c801aba084fd01063af1463
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Signed-off-by: Nicholas Lim <nicknitewolf@lineageos.org>

---
+++ system_prop.mk
@@ -0,0 +1,45 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=false
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false \
+    persist.camera.hal.debug.mask=0
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m \
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320
+
+# Factory Reset Protection
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/platform/soc.0/7824900.sdhci/by-name/frp
+
+# First api level, device has been commercially launched
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.first_api_level=22
+
+# Latest hardware revision supported
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.model.platform=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds \
+    persist.radio.custom_ecc=1 \
+    persist.radio.ecc_hard_1=998 \
+    persist.radio.ecc_hard_count=1 \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    ro.telephony.default_network=9,9
